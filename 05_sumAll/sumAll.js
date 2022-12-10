7d722df3e0320b63ed34e7f18d3213f2cdb10a9d
Tae Woo Kim <taewookim02@gmail.com>
2022-12-11 04:47:20 +0800

    Pass sumAll and leapYears test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // Guard clause to check if num1 and num2 is a number
+  if (
+    typeof num1 !== "number" ||
+    Number.isNaN(num1) ||
+    typeof num2 !== "number" ||
+    Number.isNaN(num2)
+  ) {
+    return "ERROR";
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  // create a variable to add
+  let total = 0;
 
+  // if larger integer comes first
+  const restingVariable = num2; // smaller
+  // I want to swap num1 and num2
+
+  if (num2 > num1) {
+    // from num1 to num2, loop over and add every single number
+    for (let i = 0; i <= num2; i++) {
+      total += i;
+    }
+  }
+  if (num1 > num2) {
+    for (let i = 0; i <= num1; i++) {
+      total += i;
+    }
+  }
+
+  return total;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // if year is divisble by 100, its not a leap year unless its divisble by 400
+  // for cases like 1900 which passes 4 but it has to return false
+  if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  }
+  // if year is divisble by 4, its a leap year
+  // if year is divisble by 400, its a leap year
+  if (year % 4 === 0 || year % 400 === 0) {
+    return true;
+  }
 
+  // For all the other cases
+  return false;
+
+  // return boolean
 };
 
 // Do not edit below this line
