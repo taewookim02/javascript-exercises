04c9119b0e77902fbfbca526aa28861c0c7f942c
Tae Woo Kim <taewookim02@gmail.com>
2022-12-11 01:25:37 +0800

    Solve 2 test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let result = "";
+  if (str === "") return "";
+  if (num < 0) return "ERROR";
+  for (let i = 1; i <= num; i++) {
+    result += str;
+  }
+  return result;
 };
-
+repeatString("hey", 10);
 // Do not edit below this line
 module.exports = repeatString;
