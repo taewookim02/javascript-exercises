0e4b46b7214c4da4b8a74d7d6e44570b93142c2b
Tae Woo Kim <taewookim02@gmail.com>
2022-12-11 04:18:16 +0800

    Add O(n) solution using splice and while loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,13 +5,24 @@ const removeFromArray = function (arr, ...args) {
   //   result = arr.filter((arrVal) => arrVal !== argsVal);
   // });
 
-  // go through args and loop over arr using filter and update using reduce
-  let result = args.reduce((acc, val) => {
-    return acc.filter((arrVal) => arrVal !== val);
-  }, arr);
-  return result;
-};
+  // go through args and loop over arr using filter and update using reduce O(n^2)
+  // let result = args.reduce((acc, val) => {
+  //   return acc.filter((arrVal) => arrVal !== val);
+  // }, arr);
+  // return result;
 
+  // go through args and oop over arr using splice O(n)
+  args.forEach((val) => {
+    let index = arr.indexOf(val);
+
+    while (index !== -1) {
+      arr.splice(index, 1);
+      index = arr.indexOf(val);
+    }
+  });
+  return arr;
+};
 removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+
 // Do not edit below this line
 module.exports = removeFromArray;
