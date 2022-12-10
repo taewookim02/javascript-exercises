5c92194a9a3285b5d8db42cc7f1c289f23e7f57c
Tae Woo Kim <taewookim02@gmail.com>
2022-12-11 04:58:20 +0800

    Pass tempConversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+// Convert Fahrenheit to Celsius
+const convertToCelsius = function (num) {
+  // subtract 32 and multiply by .5556
+  return +((num - 32) * 0.5556).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  // Celsius to Fahrenheit formula
+  // multiply num (fahrenheit) by 1.8 and add 32
+  // convert to integer
+  return +(num * 1.8 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
