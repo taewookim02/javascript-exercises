cde678f25574e7e48fb536318c918d338ac44a4e
Tae Woo Kim <taewookim02@gmail.com>
2022-12-20 18:18:11 +0800

    Pass calculator test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length === 0) return 0;
+
+  return arr.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, cur) => {
+    return acc * cur;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
+console.log(factorial(2));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
