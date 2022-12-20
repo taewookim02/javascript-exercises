c4cb762cc084b3c879a868804db3d36f7002c0f9
Tae Woo Kim <taewookim02@gmail.com>
2022-12-20 18:32:57 +0800

    Pass test for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const splitList = str.split("");
+  const reverseList = splitList.reverse();
+  // remove non alphabetical values on both
+  const resultStringToCompare = str
+    .split("")
+    .filter((alphabet) => alphabet.length === 1 && alphabet.match(/[a-z]/i))
+    .map((char) => char.toLowerCase())
+    .join("");
+  const reversedStringToCompare = reverseList
+    .filter((alphabet) => alphabet.length === 1 && alphabet.match(/[a-z]/i))
+    .map((char) => char.toLowerCase())
+    .join("");
 
+  // if str === reversed,joined list ? true : false
+  return resultStringToCompare === reversedStringToCompare;
 };
 
 // Do not edit below this line
