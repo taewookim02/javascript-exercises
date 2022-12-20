cb5a2b0505ab20763ae77df99903f4e1af152287
Tae Woo Kim <taewookim02@gmail.com>
2022-12-20 21:33:03 +0800

    Pass findTheOldest test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldest = people.reduce((oldest, person) => {
+    if (!oldest.yearOfDeath) {
+      oldest.yearOfDeath = new Date().getFullYear();
+    }
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const personAge = person.yearOfDeath - person.yearOfBirth;
+    return personAge > oldestAge ? person : oldest;
+  }, people[0]);
+  return oldest;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
